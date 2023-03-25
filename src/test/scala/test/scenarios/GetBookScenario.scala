cba128c82ca56199132faa0a2915f1492cad725a
Anna Bujak <ankaxx123@gmail.com>
2023-03-25 22:28:40 +0100

    New Method GetBookOnce

--- src/test/scala/test/scenarios/GetBookScenario.scala
+++ src/test/scala/test/scenarios/GetBookScenario.scala
@@ -1,10 +1,12 @@
 package test.scenarios
 import io.gatling.core.Predef.scenario
-import test.requests.{GetTokenRequest, GetBook}
+import test.requests.{GetBook, GetOnceBook, GetTokenRequest}
 import test.requests.GetBook.getBook
+import test.requests.GetOnceBook.getBook
 object GetBookScenario {
 
   val createUserScenario = scenario("Create user")
     .exec(GetTokenRequest.get_token)
     .exec(GetBook.getBook)
+    .exec(GetOnceBook.getBook)
 }
