dc5daaf691425ed99b831adfeea2ce61c444ce9e
Anna Bujak <ankaxx123@gmail.com>
2023-06-20 01:08:45 +0200

    Added aditional setup in scenario

--- src/test/scala/test/simulations/SimulationUser.scala
+++ src/test/scala/test/simulations/SimulationUser.scala
@@ -1,15 +1,19 @@
 package test.simulations
-import io.gatling.core.Predef._
+import io.gatling.core.Predef.{forAll, _}
+
 import scala.concurrent.duration._
 import test.scenarios.BookingScenario
 
 class SimulationUser extends Simulation {
 
   private val bookingScenarioExec = BookingScenario.bookingScenario.inject(
+    nothingFor(5), // Pause for a given duration
+    atOnceUsers(10),
     rampUsers(400).during(10))
 
   private val bookingScenarioAssertion= setUp(
     bookingScenarioExec).assertions(
     forAll.successfulRequests.percent.gt(95)
-  )
+  ).assertions(
+    forAll.responseTime.mean.lte(500))
 }
